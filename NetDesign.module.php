7a28b57466ccbc81f720a80c0ca66f70391e645e
Kristof Torfs <kristof.torfs@econoweb.be>
2014-09-02 05:06:04 +0200

    Updated permissions

--- NetDesign.module.php
+++ NetDesign.module.php
@@ -69,7 +69,7 @@ class NetDesign extends CMSModule {
 
     function Install() {
         // Create permission(s)
-        $this->CreatePermission('usage', 'Manage NetDesign CMS settings');
+        $this->CreatePermission('usage', 'NetDesign CMS: Manage NetDesign CMS settings');
         // Copy imagecache.php to the uploads directory
         $src = cms_join_path($this->GetModulePath(), 'imagecache.php');
         $dst = cms_join_path($this->config['uploads_path'], 'imagecache.php');
